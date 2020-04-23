fb0d160406f00db57fbff337ed518aef62303d45
HenrikDK2 <henrik265@outlook.dk>
2020-04-23 15:54:53 +0200

    Ny lektion

---
+++ IntermediateReactv2/src/components/Pet.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "@reach/router";
+export default function Pet(props) {
+  let hero = "http://placecorgi.com/300/300";
+
+  if (props.medial.length) hero = props.medial[0].small;
+  return (
+    <Link to={`/details/${props.id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={props.name} />
+      </div>
+
+      <div className="info">
+        <h1>{props.name}</h1>
+        <h2>{`${props.animal} - ${props.breed} - ${props.location}`}</h2>
+      </div>
+    </Link>
+  );
+}
